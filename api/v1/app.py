d3a1cddb6c461c5c4c928e3526938f2e01a7f3df
Jebeniani <aymenjebeniani05@gmail.com>
2022-09-19 15:30:24 +0100

    storage close call

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,16 +5,19 @@ from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def app_teardown(self):
     """method that calls storage.close()"""
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST") or '0.0.0.0',
-    port=getenv("HBNB_API_PORT") or 5000,
-    threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
