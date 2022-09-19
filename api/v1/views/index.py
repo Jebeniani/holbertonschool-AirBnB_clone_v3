a6c76b9b83cdbfb11455e106a348c24e69a5e54c
khawladaouay <daouaykhawla782@gmail.com>
2022-09-19 16:02:23 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-from os import environ
+"""
+route that returns json status response
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
 from models.amenity import Amenity
-from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 from models import storage
-from flask import jsonify
-from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def status():
-    """status view"""
-    return jsonify(
-        {
-            "status": "OK"
-        })
+@app_views.route('/status', methods=['GET'])
+def send_status():
+    """returns the status"""
+
+    resp = {'status': 'OK'}
+    return jsonify(resp)
 
 
 @app_views.route('/stats')
-def stat():
-    """stats view"""
-    return jsonify(
-        {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User)
-        })
+def show_stats():
+    """returns the count for all objects"""
+    rep = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(rep)
+
+
+@app_views.app_errorhandler(404)
+def not_found(e):
+    """Page not found."""
+    resp = {"error": "Not found"}
+    return jsonify(resp), e.code
